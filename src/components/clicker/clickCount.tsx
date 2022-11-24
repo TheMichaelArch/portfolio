839c3f59822c12e3052ab2955992b02f2c9d1272
TheMichaelArch <maja.markus.mikael@gmail.com>
2022-11-24 15:54:10 +0200

    4th Commit

---
+++ src/components/clicker/clickCount.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from '/clickCount.module.scss'
+
+type ClickCountProps = {
+
+}
+
+const ClickCount: React.FC<ClickCountProps> = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ClickCount
