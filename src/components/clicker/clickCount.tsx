4c7f8c44f7eb3cefe0e7edce7a776d945168c1be
TheMichaelArch <maja.markus.mikael@gmail.com>
2022-12-01 09:06:58 +0200

    fifth commit

--- src/components/clicker/clickCount.tsx
+++ src/components/clicker/clickCount.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import styles from '/clickCount.module.scss'
+import styles from './clickCount.module.scss'
+import { useState } from 'react';
 
 type ClickCountProps = {
 
 }
 
 const ClickCount: React.FC<ClickCountProps> = () => {
-  return (
-    <div>
-      
-    </div>
+  const [count, setCount] = useState(0);
+    return (
+      <div className={styles.container}>
+      {/* Declare a new state variable, which we'll call "count" */}
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+              <h2> Click me </h2>
+            </button>
+      </div>
   )
 }
 
