0f4ff1b7a2609bafb07a8221aac128338ec34d01
TheMichaelArch <maja.markus.mikael@gmail.com>
2022-12-15 09:49:32 +0200

    Final verdict

--- src/components/contacts/contacts.tsx
+++ src/components/contacts/contacts.tsx
@@ -10,8 +10,9 @@ const Contacts: React.FC<ContactsProps> = () => {
     <div className={styles.container}>
       <h1>Contacts</h1>
       <a href="https://www.linkedin.com/in/markus-mikael-maja/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-      <p> Discord: [Markus Maja#3230] </p>
-      <p> Gmail: maja.markus.mikael@gmail.com </p>
+      <p> Discord: [M&#97;rkus M&#97;j&#97;#3230] </p>
+      <a href="mailto:m&#97;j&#97;.m&#97;rkus.mik&#97;el@gm&#97;il.com">Email me</a>
+      <p> m&#97;j&#97;.m&#97;rkus.mik&#97;el@gm&#97;il.com </p>
 
     </div>
   )
